76d770a36a78b711d591adbe18413b314652d2b2
bkfwebdev <bryant.feld@gmail.com>
2020-09-16 23:32:01 -0400

    Updates

---
+++ precache-manifest.7052b3d303a8f861c4890f459cfab849.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b757292ca880bf5c753639df74ec7969",
+    "url": "/bkfwebdev/wwma3/index.html"
+  },
+  {
+    "revision": "3708c55985eb130b4663",
+    "url": "/bkfwebdev/wwma3/static/css/main.48aa1a7c.chunk.css"
+  },
+  {
+    "revision": "3989c329c2b33eaedbe2",
+    "url": "/bkfwebdev/wwma3/static/js/2.0a8da34e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bkfwebdev/wwma3/static/js/2.0a8da34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3708c55985eb130b4663",
+    "url": "/bkfwebdev/wwma3/static/js/main.db78b44b.chunk.js"
+  },
+  {
+    "revision": "88c6aad9725c1776677a",
+    "url": "/bkfwebdev/wwma3/static/js/runtime-main.a75ccb81.js"
+  },
+  {
+    "revision": "b24544cc752546d25ef5473956283a96",
+    "url": "/bkfwebdev/wwma3/static/media/smallwwma.b24544cc.png"
+  },
+  {
+    "revision": "4ce0d858092fc64e86882cb7e701b012",
+    "url": "/bkfwebdev/wwma3/static/media/wisdomflower.4ce0d858.jpg"
+  }
+]);
